7bfefb8c2357506946ddffbbad4aa916403687cf
Martin Reiche <reiche.stud@gmail.com>
2017-08-09 21:22:17 +0200

    Added Code of chapter one

---
+++ chapter01/js/example.js
@@ -0,0 +1,11 @@
+// hello world
+console.log("Hello from JS!");
+// calculations
+console.log(6 + 3);
+console.log(6 - 3);
+console.log(6 * 3);
+console.log(6 / 3);
+// string concatenation and numerical addition
+console.log(4+5);
+console.log("4+5");
+console.log("4" + "5");
