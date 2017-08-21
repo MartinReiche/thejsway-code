6a1340864b770f3418b18d771fb94a0ae8e1c3ba
Martin Reiche <reiche.stud@gmail.com>
2017-08-21 22:39:50 +0200

    Add code examples of chapter 21

--- chapter21/js/example.js
+++ chapter21/js/example.js
@@ -1,7 +1,6 @@
+// AJAX requests
 fetch(
-	// "https://raw.githubusercontent.com/bpesquet/thejsway/master/resources/languages.txt"
-	"https://blog.fefe.de/"
-	// "https://blog.sessionstack.com/how-javascript-works-inside-the-v8-engine-5-tips-on-how-to-write-optimized-code-ac089e62b12e"
+	"https://raw.githubusercontent.com/bpesquet/thejsway/master/resources/languages.txt"
 )
 	.then(response => response.text()) // Access and return response's text content
 	.then(text => {
@@ -13,3 +12,46 @@ fetch("http://non-existent-resource")
     console.error(err.message);
   });
 
+// Handling JSON
+
+// Define a JavaScript object
+const plane = {
+  manufacturer: "Airbus",
+  model: "A320"
+};
+console.log(plane); // Display the object
+const planeText = JSON.stringify(plane);
+console.log(planeText); // Display the object as a JSON string
+console.log(JSON.parse(planeText)); // Display the object
+
+// Define an array containing two objects
+const planes = [
+  {
+    manufacturer: "Airbus",
+    model: "A320"
+  },
+  {
+    manufacturer: "Boeing",
+    model: "737"
+  }
+];
+console.log(planes); // Display the array of objects
+const planesText = JSON.stringify(planes);
+console.log(planesText); // Display the array as a JSON string
+console.log(JSON.parse(planesText)); // Display the array of objects
+
+// fetch JSON from URL
+fetch(
+  "https://raw.githubusercontent.com/bpesquet/thejsway/master/resources/movies.json"
+)
+  .then(response => response.json()) // Access and return response's JSON content
+  .then(movies => {
+    // Iterate on the movie array
+    movies.forEach(movie => {
+      // Display title of each movie
+      console.log(movie.title);
+    });
+  })
+  .catch(err => {
+    console.error(err.message);
+  });
