605798b64d90dd9da95f955ed008e04f3822ab62
Martin Reiche <reiche.stud@gmail.com>
2017-08-29 13:22:57 +0200

    Fixed require format

--- chapter24/dates/index.js
+++ chapter24/dates/index.js
@@ -1,5 +1,5 @@
 // load moment.js
-const moment = require("./node_modules/moment/moment.js");
+const moment = require("moment");
 const now = moment();
 const date = moment("1976-11-26");
 console.log(now.format("dddd, MMMM Do YYYY"));
