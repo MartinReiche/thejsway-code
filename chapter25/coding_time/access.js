6e104d0bf2d6fb75969df283f62ba418ce4ae69c
Martin Reiche <reiche.stud@gmail.com>
2017-08-31 23:13:46 +0200

    Added remaining code exercises from chapter 25

--- chapter25/coding_time/access.js
+++ chapter25/coding_time/access.js
@@ -19,3 +19,54 @@ document.querySelector("form").addEventListener("submit", e => {
 			console.error(err.message);
 		});
 });
+
+// Country as JSON
+const countries = [
+	{
+		name: "Laos",
+		year: 2007
+	},
+	{
+		name: "China",
+		year: 2008
+	}
+];
+
+const travelData = {
+	name: "Martin",
+	countries: countries
+};
+
+// fetch post reuest
+fetch("http://localhost:3000/api/countries", {
+	method: "POST",
+	headers: {
+		accept: "application/json",
+		"Content-Type": "application/json"
+	},
+	body: JSON.stringify(travelData)
+})
+	.then(response => response.text())
+	.then(result => {
+		console.log(result);
+	})
+	.catch(err => {
+		console.error(err.message);
+	});
+
+// Article as JSON
+document.getElementById("articles").addEventListener("submit", e => {
+	e.preventDefault();
+	const formData = new FormData(e.target);
+	fetch("http://localhost:3000/api/articles", {
+		method: "POST",
+		body: formData
+	})
+		.then(response => response.text())
+		.then(result => {
+			document.getElementById("articleResults").textContent = result;
+		})
+		.catch(err => {
+			console.error(err.message);
+		});
+});
--- chapter25/coding_time/index.js
+++ chapter25/coding_time/index.js
@@ -2,11 +2,15 @@
 const express = require("express");
 // load muttler for handling form data
 const multer = require("multer");
+// load body-parser as module
+const bodyParser = require("body-parser");
 
 // Access express
 const app = express();
 // Access muttler
 const upload = multer();
+// Acces Json parser
+const jsonParser = bodyParser.json();
 
 // Enable CORS (see https://enable-cors.org/server_expressjs.html)
 app.use((req, res, next) => {
@@ -33,4 +37,20 @@ app.post("/tshirt", upload.array(), (req, res) => {
 	const size = req.body.size;
 	const color = req.body.color;
 	res.send(`Command received! Size: ${size}, color: ${color}`)
+})
+
+app.post("/api/countries", jsonParser, (req, res) => {
+	const numCountries = req.body.countries.length;
+	res.send(`Hi ${req.body.name}, you visited ${numCountries} countries. Keep traveling!`);
+})
+
+// handle post request with new article
+// create articles array
+const articles = [];
+// handle post reuest with new article form data
+app.post("/api/articles", upload.array(), (req, res) => {
+	const article = req.body;
+	// add article to articles
+	articles.push({id: articles.length + 1, title: article.title, body: article.body})
+	res.send(`New article added successfully with ID: ${articles.length}`)
 })
